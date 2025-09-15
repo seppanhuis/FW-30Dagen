aba9319d3623b1e7dfded7020d022c0cca379d18
seppanhuis <seppan2008@hotmail.com>
2025-09-15 10:44:26 +0200

    dag 7

--- routes/web.php
+++ routes/web.php
@@ -1,58 +1,21 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\job;
 
 Route::get('/', function () {
     return view('Home');
 });
 
-Route::get('/jobs', function () {
-    return view(
-        'jobs',
-        [
-            'jobs' => [
-                [
-                    'id' => 1,
-                    'title' => 'Director',
-                    'salary' => '$50.000'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Programmer',
-                    'salary' => '$10.000'
-                ],
-                [
-                    'id' => 3,
-                    'title' => 'Teacher',
-                    'salary' => '$40.000'
-                ]
-            ]
-        ]
-    );
+Route::get('/jobs', function (){
+    return view('jobs', [
+            'jobs' => job::all()
+        ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50.000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10.000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40.000'
-        ]
-    ];
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
+    $job = job::find($id);
     return view('job', ['job' => $job]);
 });
 
